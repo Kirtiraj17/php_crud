a8ae65c6098a7f5d31b9ac99e5a4d5030cba0335
gkirtirajprdxn <gkirtiraj@prdxn.com>
2021-02-18 15:34:50 +0530

    added fname and lname validation in validator

--- classes/Validator.php
+++ classes/Validator.php
@@ -12,5 +12,38 @@ class Validator
 		return $data;
 	}
 
+	public $first_name;
+	public $last_name;
+
+	// validate first name
+	public function validateFirstName($first_name) {
+
+		$this->first_name = $this->test_input($first_name);
+
+		if(empty($this->first_name)) {
+			$this->fname_error = "First name is required!";
+		} else {
+			//check for letters and spaces
+			if(!preg_match("/^[a-zA-Z-' ]*$/", $this->first_name)) {
+				$this->fname_error = "Invalid First name!";
+			}
+		}
+	}
+
+	// validate last name
+	public function validateLastName($last_name) {
+
+		$this->last_name = $this->test_input($last_name);
+
+		if(empty($this->last_name)) {
+			$this->lname_error = "Last name is required!";
+		} else {
+			//check for letters and spaces
+			if(!preg_match("/^[a-zA-Z-' ]*$/", $this->last_name)) {
+				$this->lname_error = "Invalid Last name!";
+			}
+		}
+	}
+
 
 }
