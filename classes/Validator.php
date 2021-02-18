ca4c6141cdc9e1062a008e5f777cbf9ba6eca227
gkirtirajprdxn <gkirtiraj@prdxn.com>
2021-02-18 15:33:22 +0530

    added validator class file

---
+++ classes/Validator.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once "Functions.php";
+
+class Validator
+{
+
+	public function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+
+}
