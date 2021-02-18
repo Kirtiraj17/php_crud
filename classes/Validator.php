e3949d69c0502c7d6b6c1729fb60c1903d5d32c8
gkirtirajprdxn <gkirtiraj@prdxn.com>
2021-02-18 15:39:14 +0530

    added password and cpassword validation in validator

--- classes/Validator.php
+++ classes/Validator.php
@@ -15,9 +15,13 @@ class Validator
 	public $first_name;
 	public $last_name;
 	public $email;
+	public $password;
+	public $cpassword;
 	public $fname_error='';
 	public $lname_error='';
 	public $email_error='';
+	public $password_error='';
+	public $cpassword_error='';
 
 	// validate first name
 	public function validateFirstName($first_name) {
@@ -72,6 +76,39 @@ class Validator
 		    }
 		}
 	}
+
+	// validate password
+	public function validatePassword($password) {
+
+		$this->password = $this->test_input($password);
+
+		if(empty($this->password)) {
+			$this->password_error = "Password is required!";
+		} else {
+			$uppercase = preg_match('@[A-Z]@', $password);
+			$lowercase = preg_match('@[a-z]@', $password);
+			$number    = preg_match('@[0-9]@', $password);
+			$symbol    = preg_match('@\W@', $password);
+
+			if(!$uppercase || !$lowercase || !$number || !$symbol || strlen($password) < 8) {
+				$this->password_error = "Password must contain uppercase, lowercase, number & symbol!";
+			}
+		}
+	}
+
+	// validate confirm password
+	public function validateCPassword($cpassword, $password) {
+		$this->cpassword = $this->test_input($cpassword);
+
+		if(empty($this->cpassword)) {
+			$this->cpassword_error = "Confirm password is required!";
+		} else {
+			if($cpassword != $password) {
+		    	$this->cpassword_error = "Confirm password does not matched!";
+		    }
+		}
+	}
+
 
 
 }
